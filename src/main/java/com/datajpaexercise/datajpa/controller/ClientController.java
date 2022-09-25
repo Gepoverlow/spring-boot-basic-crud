b304a822aea901590e6db60bf10360e1012be67a
lucas <gepeoverload@gmail.com>
2022-09-25 21:42:58 +0200

    added explenation on abstracted repository class calle din the controller

--- src/main/java/com/datajpaexercise/datajpa/controller/ClientController.java
+++ src/main/java/com/datajpaexercise/datajpa/controller/ClientController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RequestMapping(value = "/client")
 public class ClientController {
 
+    // This is the repository that holds our clients, and since it extends from JPArepository, it inherits some very nice methods to talk to the db
     @Autowired
     private ClientRepository clientRepository;
 
